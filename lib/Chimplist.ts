20d5d0ef090fc426a2bd94d1bf502c9c0f936774
Shikhar Sharma <ceoshikhar@gmail.com>
2020-06-30 22:01:22 +0530

    Refactor and make the code clean

--- lib/Chimplist.ts
+++ lib/Chimplist.ts
@@ -1,5 +1,4 @@
 import * as ACTIONS from './actions';
-import * as dotenv from 'dotenv';
 
 export default class Chimplist {
   private API_KEY: string;
@@ -9,8 +8,7 @@ export default class Chimplist {
     const api_key_regex = /.+\-.+/;
 
     if (!api_key_regex.test(api_key)) {
-      console.error('MISSING OF INVALID API KEY: ' + api_key);
-      throw new Error('missing or invalid api key: ' + api_key);
+      throw new Error('MISSING OR INVALID API KEY: ' + api_key);
     }
 
     this.API_KEY = api_key;
@@ -19,6 +17,9 @@ export default class Chimplist {
 
   // createList - create a new list
   createList = async (options): Promise<any> => {
+    if (options === null) {
+      throw new Error('INVALID or MISSING ARGUMENTS');
+    }
     return ACTIONS.createList(this.API_KEY, this.DATA_CENTER, options);
   };
 
@@ -92,98 +93,3 @@ export default class Chimplist {
     );
   };
 }
-
-/* THIS IS
- * FOR TESTING
- * THE CHIMPLIST Lib */
-
-dotenv.config();
-
-//@ts-ignore
-const API_KEY: string = process.env.API_KEY;
-
-const Chimp = new Chimplist(API_KEY);
-
-//@ts-ignore
-const list_id: string = process.env.LIST_ID;
-
-const options = {
-  name: 'Test List',
-  contact: {
-    company: 'Test List',
-    address1: 'Test List',
-    address2: 'Test List',
-    city: 'Test List',
-    state: 'Test List',
-    zip: 'Test List',
-    country: 'Test List',
-    phone: 'Test List'
-  },
-  permission_reminder: 'Test List',
-  campaign_defaults: {
-    from_name: 'Test List',
-    from_email: 'thisisaninvalidemail@gmail.com',
-    subject: 'Test List',
-    language: 'Test List'
-  },
-  email_type_option: true
-};
-
-// Chimp.createList(options)
-//   .then((res) => console.log('Running createList() !!', res))
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
-
-Chimp.updateList('dee60118e4', options)
-  .then((res) => console.log('Running updateList() !! ', res))
-  .catch((error) => console.log('YOU FUCKEDUP', error));
-
-// Chimp.deleteList('1e7b476120')
-//   .then((res) => console.log('Running deleeList() !!', res))
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
-
-// const options = {
-//   email_address: 'skasddjklasdja@gmail.com',
-//   status: 'subscribed',
-//   merge_fields: {
-//     FNAME: 'Baklol',
-//     LNAME: 'Pandit'
-//   }
-// };
-
-// Chimp.addMember('dee60118e4', options)
-//   .then((res) => console.log('Running addMember() !!', res))
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
-
-// Chimp.getAllMembers('dee60118e4')
-//   .then((res) =>
-//     console.log(
-//       'Running getAllMembers() !!',
-//       res.data.members[0].merge_fields.FNAME
-//     )
-//   )
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
-
-// Chimp.getMember('dee60118e4', '7de2e79e877ade00c0d02811223ab5f9')
-//   .then((res) =>
-//     console.log('Running getMember() !!', res.data.merge_fields.FNAME)
-//   )
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
-
-// const options = {
-//   merge_fields: {
-//     FNAME: 'Baklol',
-//     LNAME: 'Pandit'
-//   }
-// };
-
-// Chimp.updateMember('dee60118e4', '7de2e79e877ade00c0d02811223ab5f9', options)
-//   .then((res) => console.log('Running updateMember() !!', res))
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
-
-// Chimp.archiveMember('dee60118e4', '7de2e79e877ade00c0d02811223ab5f9')
-//   .then((res) => console.log('Running updateMember() !!', res))
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
-
-// Chimp.deleteMember('dee60118e4', '7de2e79e877ade00c0d02811223ab5f9')
-//   .then((res) => console.log('Running updateMember() !!', res))
-//   .catch((error) => console.log('YOU FUCKEDUP', error));
